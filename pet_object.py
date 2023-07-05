ed37e6d4ffa171a45c48aaf5c5b9b34ef12dcef9
Family <albon.136546120855@2depedqc.ph>
2023-07-05 21:02:15 +0800

    This program is functioning and colors are added

--- pet_object.py
+++ pet_object.py
@@ -6,26 +6,26 @@
 # import the class from module created
 from pet import Pet
 # implement functions
-def main()
+def main():
     pet = Pet()
   # ask the user for an inputs and save it all
   # name of the pet
-  name = input("Great Day! Please enter the name of your pet: ")
-  pet.set_name(name)
+    name = input("\033[93mGreat Day! Please enter the name of your pet: ")
+    pet.set_name(name)
 
   # animal type of the pet
-  animal_type = input("Now, enter the animal type of your pet: ")
-  pet.set_animal_type(animal_type)
+    animal_type = input("\033[96mNow, enter the animal type of your pet: ")
+    pet.set_animal_type(animal_type)
 
   # age of the pet
-  age = input("You're almost done! How old is your pet?: ")
-  pet.set_age(age)
+    age = input("\033[97mYou're almost done! How old is your pet?: ")
+    pet.set_age(age)
 
   # print the output
-  print("Good Job! Here is the list of information of your lovely pet:")
-  print("Name:", pet.get_name())
-  print("Animal Type:", pet.get_animal_type())
-  print("Age:", pet.get_age())
+    print("\033[95mGood Job! Here is the list of information of your lovely pet:")
+    print("\033[91mName:", pet.get_name())
+    print("\033[92mAnimal Type:", pet.get_animal_type())
+    print("\033[94mAge:", pet.get_age())
 
 # check for errors
 if __name__ == "__main__":
