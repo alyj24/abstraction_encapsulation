0648980a7d263ff5c80b68c39696b4671f01b63b
Family <albon.136546120855@2depedqc.ph>
2023-07-05 20:13:09 +0800

    Car Object file is created and its Pseudocode

---
+++ car_object.py
@@ -0,0 +1,12 @@
+# design a program that creates a Car object then calls the accelerate method five times.
+# after each call to the accelerate method, get the current speed of the car and display it.
+# then, call the brake method for five times.
+# after each call to the brake method, get the current speed of the car and display it.
+
+# pseudocode
+# import class from the module created
+# implement functions
+  # establish the object of the car
+  # call the program to accelerate, five times
+  # call the program to brake, five times
+  # check for errors
