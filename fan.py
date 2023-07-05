41942793000b1b0b68180e88e545bd0c52b9d067
Family <albon.136546120855@2depedqc.ph>
2023-07-05 19:04:53 +0800

    Introduction and Pseudocode

---
+++ fan.py
@@ -0,0 +1,16 @@
+print("Assignment 9".center(42, "="))
+print("=" * 47)
+print("CMPE-103-MODULE-06-ABSTRACTION-AND-ENCAPSULATION")
+print("=" * 47)
+
+# design a class named "Fan" to represent a fan, then try and run the program.
+
+# pseudocode
+# create the class that will represent a fan
+  # implement the fan speed
+  # establish the constructor
+  # construct the accessor and mutator methods for all four private data fields
+  # speed of the fan
+  # on-switch of the fan
+  # radius of the fan
+  # color of the fan
