23fa61b214231a5421a9c293e68feb4b0629be0b
Family <albon.136546120855@2depedqc.ph>
2023-07-05 19:29:06 +0800

    TestFan program and its Pseudocode

---
+++ TestFan.py
@@ -0,0 +1,11 @@
+# write a test program named TestFan that creates two Fan objects.
+
+# pseudocode
+# import the class from module created
+# create the TestFan class
+  # implement the class contains
+    # establish the objects of two fans
+    # recognize the command of first fan and print the output
+    # recognize the command of second fan and print the output
+
+# run the program
