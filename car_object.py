fd061a74cdd8f6254f57ac59412dbe98defcc9f3
Family <albon.136546120855@2depedqc.ph>
2023-07-05 20:23:55 +0800

    Another line of pseudocode and output command are added

--- car_object.py
+++ car_object.py
@@ -14,6 +14,15 @@ def main():
   # call the program to accelerate, five times
   for i in range(5):
       car.accelerate()
-      
+      # print the output
+      print("After the acceleration of the car~")
+      print("The current speed of Chevrolet Equinox is ", car.get_speed())
+
   # call the program to brake, five times
+  for i in range(5):
+      car.brake()
+      # print the output
+      print("After the brake of the car~")
+      print("The current speed of Chevrolet Equinox is ", car.get_speed())
+
   # check for errors
