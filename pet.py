4a6563c2796e7543e7d674ce232215c26fc270ff
Family <albon.136546120855@2depedqc.ph>
2023-07-05 20:37:00 +0800

    Pet class is created and its Pseudocode

---
+++ pet.py
@@ -0,0 +1,13 @@
+# write a class named 'Pet' with the given data attributes
+'''Methods: set_name(), set_animal_type(), set_age(), get_name(), get_animal_type(), and get_age()'''
+
+# pseudocode
+# create the class
+  # implement the constructor
+  # establish the methods for the pet
+  # assigns a value to the name field
+  # assigns a value to the animal type field
+  # assigns a value to the age field
+  # return the value of the name field
+  # return the value of the animal type field
+  # return the value of the age field
