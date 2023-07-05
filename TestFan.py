3a79d1a89deede6a35e6f261b55679b2dff022bf
Family <albon.136546120855@2depedqc.ph>
2023-07-05 19:37:29 +0800

    First Fan

--- TestFan.py
+++ TestFan.py
@@ -10,8 +10,14 @@ class TestFan:
         # establish the objects of two fans
         first_fan = Fan(Fan.FAST, True, 10, 'yellow')
         second_fan = Fan(Fan.MEDIUM, False, 5, 'blue')
-       
+
     # recognize the command of first fan and print the output
+    print("The First Fan has the following properties:")
+    print("Speed:", first_fan.get_speed())
+    print("Radius:", first_fan.get_radius())
+    print("Color:", first_fan.get_color())
+    print("Is the fan on?:", first_fan.is_on())
+   
     # recognize the command of second fan and print the output
 
 # run the program
