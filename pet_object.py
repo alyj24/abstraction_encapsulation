d32cb92144748b1cd21dae13e2332069efcfb857
Family <albon.136546120855@2depedqc.ph>
2023-07-05 20:50:22 +0800

    This pet program is created and its pseudocode

---
+++ pet_object.py
@@ -0,0 +1,13 @@
+# write a program that creates an object of the class,
+# prompts the user to enter the name, type, and age of his or her pet,
+# store the data, use object's acccessor and display.
+
+# pseudocode
+# import the class from module created
+# implement functions
+  # ask the user for an inputs and save it all
+  # name of the pet
+  # animal type of the pet
+  # age of the pet
+  # print the output
+# check for errors
