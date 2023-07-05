3ad5d54d92188f2d2b48b25cec4aa9db3a9f2146
Family <albon.136546120855@2depedqc.ph>
2023-07-05 19:55:50 +0800

    Program is good and colors are added

--- TestFan.py
+++ TestFan.py
@@ -12,18 +12,18 @@ class TestFan:
         second_fan = Fan(Fan.MEDIUM, False, 5, 'blue')
 
         # recognize the command of first fan and print the output
-        print("\033[97mThe First Fan has the following properties: ")
-        print("\033[93mSpeed:" , first_fan.get_speed())
-        print("\033[94mRadius:", first_fan.get_radius())
-        print("\033[92mColor:", first_fan.get_color())
-        print("\033[96mIs the fan on?:", first_fan.is_on())
+        print("\033[91mThe First Fan has the following properties: ")
+        print("\033[92mSpeed:" , first_fan.get_speed())
+        print("\033[95mRadius:", first_fan.get_radius())
+        print("\033[93mColor:", first_fan.get_color())
+        print("\033[97mIs the fan on?:", first_fan.is_on())
 
         # recognize the command of second fan and print the output
-        print("\033[97mThe Second Fan has the following properties: ")
-        print("\033[91mSpeed:", second_fan.get_speed())
+        print("\033[91mThe Second Fan has the following properties: ")
+        print("\033[96mSpeed:", second_fan.get_speed())
         print("\033[92mRadius:", second_fan.get_radius())
         print("\033[94mColor:", second_fan.get_color())
-        print("\033[95mIs the fan on?:", second_fan.is_on())
+        print("\033[97mIs the fan on?:", second_fan.is_on())
 
 # run the program
 test = TestFan()
