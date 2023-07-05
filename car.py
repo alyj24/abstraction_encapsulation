38820b06bae5bd5da9fde15b3bafbc4470ab9853
Family <albon.136546120855@2depedqc.ph>
2023-07-05 20:03:58 +0800

    Init Method

--- car.py
+++ car.py
@@ -5,6 +5,12 @@
 # create the class
 class Car:
   # implement the constructor
+  '''Use the __init__ method.'''
+  def __init__(self, year_model, make):
+      self.__year_model = year_model
+      self.__make = make
+      self.__speed = 0
+      
   # establish the methods for car
   # set the car's speed in increase of 5
   # set the car's speed in decrease of 5
