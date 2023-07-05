0e586be94d62f1e0fdd061bb23649cfec5061ced
Family <albon.136546120855@2depedqc.ph>
2023-07-05 20:01:20 +0800

    Class Car is created and its Pseudocode

---
+++ car.py
@@ -0,0 +1,10 @@
+# write a class named 'Car'  with the given data attributes
+'''Methods: accelerate(), brake(), and get_speed().'''
+
+# pseudocode
+# create the class
+  # implement the constructor
+  # establish the methods for car
+  # set the car's speed in increase of 5
+  # set the car's speed in decrease of 5
+  # method to get the current speed
