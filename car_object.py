aa3f3ae0d658027dac57fbf03ea34cb4dbf500d9
Family <albon.136546120855@2depedqc.ph>
2023-07-05 20:25:17 +0800

    Checking for Errors

--- car_object.py
+++ car_object.py
@@ -11,18 +11,21 @@ def main():
     # establish the object of the car
     car = Car(2005, "Chevrolet Equinox")
 
-  # call the program to accelerate, five times
-  for i in range(5):
-      car.accelerate()
-      # print the output
-      print("After the acceleration of the car~")
-      print("The current speed of Chevrolet Equinox is ", car.get_speed())
+    # call the program to accelerate, five times
+    for i in range(5):
+        car.accelerate()
+        # print the output
+        print("After the acceleration of the car~")
+        print("The current speed of Chevrolet Equinox is ", car.get_speed())
 
-  # call the program to brake, five times
-  for i in range(5):
-      car.brake()
-      # print the output
-      print("After the brake of the car~")
-      print("The current speed of Chevrolet Equinox is ", car.get_speed())
+    # call the program to brake, five times
+    for i in range(5):
+        car.brake()
+        # print the output
+        print("After the brake of the car~")
+        print("The current speed of Chevrolet Equinox is ", car.get_speed())
 
-  # check for errors
+    # check for errors
+    if __name__ == "__main__"
+        main()
+
